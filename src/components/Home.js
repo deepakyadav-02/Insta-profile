b0b5d69798fefb126d51e66da1c61c05febd884d
Deepak Yadav <deepakyadav101002@gmail.com>
2022-08-19 00:54:03 +0530

    modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,17 @@
 import { async } from "@firebase/util";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../context/UserAuthContext";
 import InstagramEmbed from "react-instagram-embed";
+import axios from "axios";
 
 // FEED
 
 const Home = () => {
   const { logOut, user } = useUserAuth();
   const navigate = useNavigate();
+  const [dd, setDd] = useState();
   const handleLogout = async () => {
     try {
       await logOut();
@@ -18,25 +20,35 @@ const Home = () => {
       console.log(error.message);
     }
   };
+
+  const fetchDAta = async () => {
+    const mediaid = "5461332850598784";
+    const access_token =
+      "IGQVJVeVctQk5fTjBGS1FqNE5BdXpCSms3clp6VGFvZA3lsYzFmUXRDemYyNzZAPemRxZAE9QdU1pSTBRUHo2MHlDRVd4RkNoY3hUT3o4RGlqY0RjVmlfd0FfUXNSMnMyT0w2MnFkUURuSldaSk9fbW0tWgZDZD";
+
+    const { data } = await axios.get(
+      `https://graph.instagram.com/${mediaid}?fields=id,media_type,media_url,username,timestamp&access_token=${access_token}`
+    );
+
+    console.log(data);
+    setDd(data);
+  };
+
+  useEffect(() => {
+    fetchDAta();
+  });
+
   return (
     <>
       {/* <div className="p-4 box mt-3 text-center">
         Hello Welcome <br />
         {user && user.email}
       </div> */}
-      <InstagramEmbed
-        url="https://instagr.am/p/Zw9o4/"
-        clientAccessToken="407198014833921|IGQVJVeVctQk5fTjBGS1FqNE5BdXpCSms3clp6VGFvZA3lsYzFmUXRDemYyNzZAPemRxZAE9QdU1pSTBRUHo2MHlDRVd4RkNoY3hUT3o4RGlqY0RjVmlfd0FfUXNSMnMyT0w2MnFkUURuSldaSk9fbW0tWgZDZD"
-        maxWidth={320}
-        hideCaption={false}
-        containerTagName="div"
-        protocol=""
-        injectScript
-        onLoading={() => {}}
-        onSuccess={() => {}}
-        onAfterRender={() => {}}
-        onFailure={() => {}}
-      />
+      <div>
+        {dd.map((d) => {
+          <div>{d.image}</div>;
+        })}
+      </div>
       <div className="d-grid gap-2">
         <Button variant="primary" onClick={handleLogout}>
           Log out
